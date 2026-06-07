ef95863e08c40e087a90ac7d8caa7708a4c98411
sam4740 <sam4740@gmail.com>
2026-06-07 13:49:47 -0400

    First commit

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+namespace LibraryManagment.Api.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCheckedOut { get; set; }
+    }
+}
---
+++ Models/Patron.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagment.Api.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
